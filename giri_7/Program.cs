622182a6e79c6cac7adf0d857708a86047d674ac
Sergey <diuk@inbox.ru>
2023-01-16 20:29:26 +0300

    коммит - max =

---
+++ giri_7/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 5;
+int b = 8;
+int c = 24;
+int d = 9;
+int e = 1;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
